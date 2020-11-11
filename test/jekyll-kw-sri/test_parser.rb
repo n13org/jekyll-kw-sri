f06aa7dafe95683846025230edf7422a4fe39761
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-11-11 08:39:28 +0100

    Add some scss test files

--- test/jekyll-kw-sri/test_parser.rb
+++ test/jekyll-kw-sri/test_parser.rb
@@ -106,31 +106,49 @@ module Jekyll
         def test_default_hash
           parser = Jekyll::KargWare::Integrity::Parser.new
 
-          assert_equal('sha384-CAKQudMhH2dRDctuCuB04YZ7U2+ypwniuzywkMd0dRD4Jc1VRwYs/qtVgempt84u', parser.calc_integrity('dummy.scss', 'Nicolas Karg'))
+          assert_equal('sha384-rmBgLB2GNUCHgb0+M0Ccnxu4UANpgScLqMdc/xUrlfzLBbGHzlYfPdNd/8pahyyS', parser.calc_integrity('/folder/any-file.scss', 'Some CSS, SASS or SCSS data'))
+        end
+
+        def test_default_hash_with_tmp_file
+          parser = Jekyll::KargWare::Integrity::Parser.new('createTmpfile' => true)
+
+          assert_equal('sha384-NEbf2o7Y2GATnYHu1V66qX824UuFHjhs0KFcHJ0dhosIoYbSMjICUH/Fx4aCU7eW', parser.calc_integrity('/test/assets/css/test-generated.scss', 'Some CSS, SASS or SCSS data'))
+
+          # Exits the temp file
+          target_tmp_file = File.join(File.dirname(__FILE__), '..', 'assets', 'css', 'test-generated.scss.tmp')
+          assert_equal true, File.exist?(target_tmp_file)
+
+          # Compare file content
+          tmp_file_content = File.read(target_tmp_file)
+          content = "Some CSS, SASS or SCSS data\n/*# sourceMappingURL=test-generated.css.map */"
+          assert_equal content, tmp_file_content
+
+          # Clean up
+          File.delete(target_tmp_file)
         end
 
         def test_sha256_hash
           parser = Jekyll::KargWare::Integrity::Parser.new('hashType' => 'sha256')
 
-          assert_equal('sha256-wL4u9IuX2Fd1BE7HRjxXaDROMcwqnKHjdvGln2LmWGE=', parser.calc_integrity('dummy.scss', 'Nicolas Karg'))
+          assert_equal('sha256-qcdjYphy3ThHSS33e6PKE+0+nMH2TO+N5TfISpOQZEI=', parser.calc_integrity('/folder/any-file.scss', 'Some CSS, SASS or SCSS data'))
         end
 
         def test_sha256_hash_no_map
           parser = Jekyll::KargWare::Integrity::Parser.new('hashType' => 'sha256', 'writeSourceMappingURL' => false)
 
-          assert_equal('sha256-RpSGJ/7qKTsHq3mtAO8hMgmu8Wek/BTBP94eKn0wmXM=', parser.calc_integrity('dummy.scss', 'Nicolas Karg'))
+          assert_equal('sha256-HAIlc6vs074SVb8er2CJeXGT35yadAC20fUVgsB18RY=', parser.calc_integrity('/folder/any-file.scss', 'Some CSS, SASS or SCSS data'))
         end
 
         def test_sha384_hash
           parser = Jekyll::KargWare::Integrity::Parser.new('hashType' => 'sha384')
 
-          assert_equal('sha384-CAKQudMhH2dRDctuCuB04YZ7U2+ypwniuzywkMd0dRD4Jc1VRwYs/qtVgempt84u', parser.calc_integrity('dummy.scss', 'Nicolas Karg'))
+          assert_equal('sha384-rmBgLB2GNUCHgb0+M0Ccnxu4UANpgScLqMdc/xUrlfzLBbGHzlYfPdNd/8pahyyS', parser.calc_integrity('/folder/any-file.scss', 'Some CSS, SASS or SCSS data'))
         end
 
         def test_sha512_hash
           parser = Jekyll::KargWare::Integrity::Parser.new('hashType' => 'sha512')
 
-          assert_equal('sha512-Tqwtzv86jf17eulxL2QQ0td1imrXRk8/G2Wojy/slX2Od2p5MBB0u8xzk0UroNEzjI7s76z0eGLZuwOJCoBU6Q==', parser.calc_integrity('dummy.scss', 'Nicolas Karg'))
+          assert_equal('sha512-WD/EQwoOsJGPdAoxtkE64MUgiALqwZ46OidB7S9oSQ+2K6roNugrNB3q8PNIp1C29f7jCneERb3xenfe6jBUgQ==', parser.calc_integrity('/folder/any-file.scss', 'Some CSS, SASS or SCSS data'))
         end
       end
     end
