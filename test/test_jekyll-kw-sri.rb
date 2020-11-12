f04c7d6c23357658de154b10e9801ef392161a5f
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-11-12 05:45:07 +0100

    Add custom tag tests

--- test/test_jekyll-kw-sri.rb
+++ test/test_jekyll-kw-sri.rb
@@ -1,25 +1,37 @@
 # frozen_string_literal: true
 
 require 'jekyll'
-# require 'jekyll-helper'
+# require_relative '../testbroken/jekyll/helper'
 
 require 'jekyll-kw-sri'
+require 'jekyll-kw-sri/configuration'
+# require 'jekyll-kw-sri/converter'
+require 'jekyll-kw-sri/parser'
 
-# class TestParser2 < JekyllUnitTest
-# def test_Nico
-#     # Liquid::Template.register_tag('sri_scss_hash', Jekyll::KargWare::SriScssHashTag)
-#     template2 = Liquid::Template.parse('{% sri_scss_hash Nico.scss %}').render!()
-#     assert_equal("Hallo", template2)
+# Test class
+class TestParser2 < Minitest::Test
+  # class TestParser2 < JekyllUnitTest
+  def test_nico
+    # converter = Jekyll::KargWare::Integrity::Converter.new
+    # assert_equal '_sass', converter.convert_extended
 
-#     # https://github.com/jekyll/jekyll/blob/9f8ac4eb7afc8e5cc47deced4cbe2c56df9d4266/test/test_tags.rb
+    # @site = Jekyll::Site.new(Jekyll::Configuration::DEFAULTS)
+    # @context = Liquid::Context.new({}, {}, { site: @site })
+    # site = Jekyll::Site.new(site_configuration)
 
-#     # var tag = Jekyll::KargWare::SriScssHashTag.new ("", "nico.css", "")
-#     # var render = tag.render(@context)
+    # Liquid::Template.register_tag('sri_scss_hash', Jekyll::KargWare::SriScssHashTag)
+    template2 = Liquid::Template.parse('{% sri_scss_hash /test/assets/css/test-dummy.scss %}').render!
+    assert_equal('sha384-UjOLM/hnGPBBNf7ocV6PJ38/j2/U3Q+t5uR8bl2FiuKrr+9PSBNJ+g00gfQslcoL', template2)
 
-#     # template = Liquid::Template.parse('{% raw %}Hallo{% endraw %}')
-#     # assert_equal("Hallo", template)
+    # https://github.com/jekyll/jekyll/blob/9f8ac4eb7afc8e5cc47deced4cbe2c56df9d4266/test/test_tags.rb
 
-#     # t = Template.new
-#     # t.parse('{%comment%}{%endcomment%}')
-# end
-# end
+    # var tag = Jekyll::KargWare::SriScssHashTag.new ("", "nico.css", "")
+    # var render = tag.render(@context)
+
+    # template = Liquid::Template.parse('{% raw %}Hallo{% endraw %}')
+    # assert_equal("Hallo", template)
+
+    # t = Template.new
+    # t.parse('{%comment%}{%endcomment%}')
+  end
+end
